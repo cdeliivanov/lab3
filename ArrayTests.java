1169d1fa32e31281235930a777b24a3ed1c3be9f
cdeliivanov <cdeliivanov@ucsd.edu>
2023-04-24 10:01:02 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,16 @@ public class ArrayTests {
     int[] input1 = {3, 4};
     assertArrayEquals(new int[]{4, 3}, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAWL() {
+    double[] input1 = {5.0, 10.0, 15.0, 0.0, 0.0};
+    assertEquals(10.0, ArrayExamples.averageWithoutLowest(input1), 0.0);
+  }
+
+  @Test
+  public void testAWL2() {
+    double[] input1 = {4.0, 4.0, 4.2, 4.2};
+    assertEquals(4.2, ArrayExamples.averageWithoutLowest(input1), 0.0);
+  }
 }
